4c897084926723ecfd310ba71d866fc15abd18ce
Ernst van der Heijden (MBP17) <ernst@basisworks.nl>
2023-02-15 13:56:16 +0100

    Rights

--- shared/packages/relations/fragments/relationlist.py
+++ shared/packages/relations/fragments/relationlist.py
@@ -17,6 +17,7 @@ class Relationlist(GlobalList):
         self.paginatesize_overwrite = ''
         self.displayfield = 'relationname'
         self.successurl_decoded = "/core/fragment/?level=" + self.level + "&package=" + self.package + "&fragment=" + self.fragment + "&pk=" + str(self.pk) + "&fragmentrefresh=data_" + self.fragment + "&refreshtarget=data&page=1"
+
         if request.user.has_perm('relations.add_relation'):
             self.tools.update({
                 'create': {
