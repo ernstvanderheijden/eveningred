f7e072ca630f69e91d8f70c5a2654281ff93bf9a
Ernst van der Heijden (MBP17) <ernst@basisworks.nl>
2023-03-01 20:37:49 +0100

    Overviews

--- core/globals/global_fragments.py
+++ core/globals/global_fragments.py
@@ -51,11 +51,12 @@ class GlobalGrid:
         self.today = datetime.today().date()
         self.weeks = request.GET.get('weeks', 2)
         self.days_in_front = configuration.days_in_front
-        weekday = datetime.today().weekday()
         if request.GET.get('searchdate', ''):
             self.searchdate = datetime.strptime(request.GET.get('searchdate'), "%Y-%m-%d").date()
+            weekday = self.searchdate.weekday()
             self.startdate = datetime.strptime(request.GET.get('searchdate'), "%Y-%m-%d").date() - timedelta(days=self.days_in_front + weekday)
         else:
+            weekday = datetime.today().weekday()
             self.startdate = self.today - timedelta(days=self.days_in_front + weekday)
             self.searchdate = self.today
         self.enddate = self.searchdate + timedelta(days=(7 * self.weeks) - weekday - 1)
